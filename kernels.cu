95d5614a2e9e1f5a91dbde9524aceb83f1d2bce2
ben <bbitmaster@gmail.com>
2012-10-28 12:33:08 -0700

    A lame fire affect is produced...

--- kernels.cu
+++ kernels.cu
@@ -3,29 +3,37 @@
 #include <curand_kernel.h>
 using namespace std;
 
-__global__ void do_fire(unsigned int *pData,curandState *state,float *firebuff[2]);
+__global__ void do_fire(unsigned int *pData,curandState *state,float *firesrc,float *firedest);
 __global__ void setup_kernel(curandState *state);
 curandState *devStates;
 
 float *firebuff[2];
+int flipstate;
 
 int init_cuda(){
 
-	cudaMalloc((void **)&firebuff[1], 600 * 800 *
+	cudaMalloc((void **)&firebuff[0], 600 * 800 *
 	                  sizeof(float));
 
-	cudaMalloc((void **)&firebuff[2], 600 * 800 *
+	cudaMalloc((void **)&firebuff[1], 600 * 800 *
 	                  sizeof(float));
 
 	cudaMalloc((void **)&devStates, 600 * 800 *
 	                  sizeof(curandState));
 	setup_kernel<<<600,800>>>(devStates);
 
+	flipstate=0;
 	return 0;
 }
 
 int run_fire(unsigned int *pData){
-	do_fire<<<600,800>>>(pData,devStates,firebuff);
+	if(flipstate){
+		do_fire<<<600,800>>>(pData,devStates,firebuff[0],firebuff[1]);
+		flipstate = 0;
+	} else {
+		do_fire<<<600,800>>>(pData,devStates,firebuff[1],firebuff[0]);
+		flipstate = 1;
+	}
 	return 0;
 }
 
@@ -37,7 +45,7 @@ __global__ void setup_kernel(curandState *state)
     curand_init(1234, id, 0, &state[id]);
 }
 
-__global__ void do_fire(unsigned int *pData,curandState *state,float *firebuff[2]){
+__global__ void do_fire(unsigned int *pData,curandState *state,float *firesrc,float *firedest){
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	int thread_y = blockIdx.x;
 	int thread_x = threadIdx.x;
@@ -51,12 +59,22 @@ __global__ void do_fire(unsigned int *pData,curandState *state,float *firebuff[2
 	state[idx] = localState;
 
 	if(thread_y >= 600-1)
-		pData[idx] = (rand << 16);
+		firedest[idx] = rand;
 	__syncthreads();
 
+
 	if(thread_y < 600-1){
-		pData[thread_y*maxwidth + thread_x] = pData[(thread_y+1)*maxwidth + thread_x];
+		float avg[4];
+		if((thread_x-1) >= 0)avg[1] = firesrc[(thread_y+1)*maxwidth + thread_x-1];
+		avg[2] = firesrc[(thread_y+1)*maxwidth + thread_x];
+		if((thread_x+1) < 800)avg[3] = firesrc[(thread_y+1)*maxwidth + thread_x+1];
+
+		avg[0] = (avg[1] + avg[2] + avg[3])/3;
+
+		firedest[thread_y*maxwidth + thread_x] = avg[0];
 	}
+
+	pData[idx] = (((int)firedest[idx])&0xFF) << 16;
 
 	return;
 }
